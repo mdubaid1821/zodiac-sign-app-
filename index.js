3c422bd5a28ec9048612276ffd4d162dda4d977a
DarkStar <113847822+mdubaid1821@users.noreply.github.com>
2023-06-27 15:53:44 +0530

    vscode
    Created a Zodiac_sign finder app.
    This Web-App uses the the birthdate and month to give your Zodiac Sign..
    It is a basic Sign finder app built using pure javascript

---
+++ index.js
@@ -0,0 +1,75 @@
+
+
+
+const zodianSign = (day , month)=>{
+        if ((month === 1 && day >= 20) || (month === 2 && day <= 18)) {
+
+          return "Aquarius";
+
+        } else if ((month === 2 && day >= 19) || (month === 3 && day <= 20)) {
+
+          return "Pisces";
+
+        } else if ((month === 3 && day >= 21) || (month === 4 && day <= 19)) {
+
+          return "Aries";
+
+        } else if ((month === 4 && day >= 20) || (month === 5 && day <= 20)) {
+
+          return "Taurus";
+
+        } else if ((month === 5 && day >= 21) || (month === 6 && day <= 20)) {
+
+          return "Gemini";
+
+        } else if ((month === 6 && day >= 21) || (month === 7 && day <= 22)) {
+
+          return "Cancer";
+
+        } else if ((month === 7 && day >= 23) || (month === 8 && day <= 22)) {
+
+          return "Leo";
+
+        } else if ((month === 8 && day >= 23) || (month === 9 && day <= 22)) {
+
+          return "Virgo";
+
+        } else if ((month === 9 && day >= 23) || (month === 10 && day <= 22)) {
+
+          return "Libra";
+
+        } else if ((month === 10 && day >= 23) || (month === 11 && day <= 21)) {
+
+          return "Scorpio";
+
+        } else if ((month === 11 && day >= 22) || (month === 12 && day <= 21)) {
+
+          return "Sagittarius";
+
+        } else if ((month === 12 && day >= 22) || (month === 1 && day <= 19)) {
+
+          return "Capricorn";
+
+        } else {
+
+          return "Invalid date";
+
+        }
+      }
+
+
+      const storeDateAndMonth = ()=>{
+        let birthdate = document.getElementById("Date").value
+        let date = new Date(birthdate)
+        let day = date.getDate()
+        let month = date.getMonth()+1
+    
+        let Sign = zodianSign(day, month)
+        document.getElementById("result").innerHTML = Sign;
+        
+
+    
+    }
+
+     
+      
